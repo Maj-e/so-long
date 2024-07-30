a0b68420739c962356e2629de888d7a177f18ce4
Matteo Lucien Jeannin Rocco <mjeannin@c3r4s1.42malaga.com>
2024-07-30 20:44:36 +0200

    png 64px added

--- src/utils/error.c
+++ src/utils/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoore-a <smoore-a@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: mjeannin <mjeannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:08:27 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/05/22 19:42:29 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/07/30 17:22:46 by mjeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_error(t_data *data, char *message)
 	if (message && !(data->map.fn))
 		ft_printf("Error\n%s\n", message);
 	else if (message && data->map.fn)
-		ft_printf("Error\n%s\nFile\n%s\n", message, data->map.fn);
+		ft_printf("Error\n%s\nFile: %s\n", message, data->map.fn);
 	else if (data->map.fn && !message)
-		ft_printf("Error\n%s\nFile\n%s\n", strerror(errno), data->map.fn);
+		ft_printf("Error\n%s\nFile: %s\n", strerror(errno), data->map.fn);
 	else
 		ft_printf("Error\n%s\n", strerror(errno));
 	terminate(data);
